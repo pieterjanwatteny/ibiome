73b0e1a9dd96b1bd86e010f2ff334aed47e9b2cf
Unknown <pieterjanwatteny@gmail.com>
2019-05-04 21:20:17 +0200

    kuuh

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,16 +4,15 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init() {
     console.log("init");
-    getData();
+    
 }
 
 const url = "https://ibiome.herokuapp.com"
 
-function getData(){
 fetch(`${url}/api/data`)
     .then(res => res.json())
     .then(data => showData(data))
-}
+
 
 function showData(plantData) {
     console.log(plantData);
