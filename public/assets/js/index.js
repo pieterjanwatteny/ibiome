052b9e9c79aa0afed90a53faeab0e4d3d07b4a82
Unknown <pieterjanwatteny@gmail.com>
2019-05-04 21:22:21 +0200

    regv

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,15 +4,16 @@ document.addEventListener("DOMContentLoaded", init)
 
 function init() {
     console.log("init");
-    
+    getjsonplantsData()
 }
 
 const url = "https://ibiome.herokuapp.com"
 
+function getjsonplantsData(){
 fetch(`${url}/api/data`)
     .then(res => res.json())
     .then(data => showData(data))
-
+}
 
 function showData(plantData) {
     console.log(plantData);
