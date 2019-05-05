2f23a20def67ddfcf086c48a5401e904f87b3d95
Pieter-Jan <pieterjanwatteny@gmail.com>
2019-05-05 16:15:56 +0200

    ma tis chill wi tzoe moetn gefixed zijn. localForga tussen de fetch en display

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,29 +8,30 @@ function init() {
 }
 
 const url = "https://ibiome.herokuapp.com"
-const store =localforage.createInstance({ name: "IBiome"})
+const store = localforage.createInstance({ name: "IBiome" })
 function getjsonplantsData() {
     fetch(`${url}/api/data`)
         .then(res => res.json())
         .then(data => storeLocalForage(data))
 }
 
-function storeLocalForage(data){
-    let plntdata= data
-    let storedata= [{ id: 1, Temperature: plntdata.Temp},
-                    { id: 2, Humidity: plntdata.Humid}];
-    store.setItem("Plantdata", storedata).then(function(value){
-        console.log("Data stored",value)
+function storeLocalForage(data) {
+    let plntdata = data
+    let storedata = [{ id: 1, Temperature: plntdata.Temp },
+    { id: 2, Humidity: plntdata.Humid }];
+    store.setItem("Plantdata", storedata).then(function (value) {
+        console.log("Data stored", value)
     })
 }
-function logLocals(){
-    store.getItem("Plantdata").then(function(value){
-        console.log("Data retrieved : " ,value);
-        console.log(value[0].Temperature)
-        document.querySelector("#Temperature").innerHTML = (""+value[0].Temperature + " °C");
+function logLocals() {
+    store.getItem("Plantdata").then(function (value) {
+        console.log("Data retrieved : ", value);
+        document.querySelector("#Temperature").innerHTML = ("" + value[0].Temperature + " °C");
+        document.querySelector("#Humidity").innerHTML = ("" + value[1].Humidity + " %");
     });
+
+
     
-   
-    document.querySelector("#Humidity").innerHTML = (""+data.Humid + " %");   
-    
+
+
 }
